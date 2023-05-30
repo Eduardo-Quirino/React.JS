e6376836a2ec3d9f77a529f324a05124121b3611
Eduardo Quirino <edu.3532quirino@gmail.com>
2023-05-30 16:14:39 -0300

    add imagem

--- app01-primeiro/src/App.js
+++ app01-primeiro/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import "./index.css"
 
 export default function App() {
+  {/*Função */ }
+  const canal = () => {
+    return ('CFB Cursos')
+  }
+
+  const curso = 'React.JS'
   return (
     <main>
-      <h1>Desenvolvedor Eduardo Quirino</h1>
-      <p>Programação de futuro.</p>
+      <header>
+        <h1>Desenvolvedor Eduardo Quirino</h1>
+        <p>Programação de futuro.</p>
+        <h4>{'Curso: ' + curso}</h4>
+        <p>{canal()}</p> {/* chama função*/}
+      </header>
+      <section>
+        <img src="./_img/40 Workstation Setups That We Really Like.jpeg" />
+        <img src="./_img/Armário de Escritório_ +56 Ideias para Organizar seu Home Office.jpeg" />
+      </section>
+
+      {/* <img src={Escritorio2} /> */}
 
     </main>
   )
